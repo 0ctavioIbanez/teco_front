f43ffd3dd78042dfa5fd957e17f4ed7c4c2fb78e
0ctavioibanez <octaviogd@live.com.mx>
2022-07-24 00:32:57 -0500

    nav icons and home card done

--- src/components/public/navbar/Navbar.jsx
+++ src/components/public/navbar/Navbar.jsx
@@ -1,19 +1,29 @@
 import { Link } from 'react-router-dom';
 import './navbar.css';
+import home from '../../../assets/img/icons/home.svg';
+import search from '../../../assets/img/icons/search.svg';
+import cart from '../../../assets/img/icons/cart.svg';
+import menu from '../../../assets/img/icons/menu.svg';
 
 const Navbar = ({ expanded, setExpanded }) => {
+  const path = window.location.pathname;
+
   return (
     <div className='navbar__ d-flex align-items-center'>
-      <Link className="navbar__item" to='/'>
+      <Link className={`navbar__item ${path === '/' ? 'active' : ''}`} to='/'>
+        <img src={home} alt="" />
         Inicio
       </Link>
-      <button className='navbar__item'>
+      <button className={`navbar__item ${path === '/search' ? 'active' : ''}`}>
+        <img src={search} alt="" />
         Buscar
       </button>
-      <Link className="navbar__item" to='/'>
+      <Link className={`navbar__item ${path === '/cart' ? 'active' : ''}`} to='/'>
+        <img src={cart} alt="" />
         Carrito
       </Link>
-      <button className="navbar__item" to='/' onClick={e => setExpanded(!expanded)}>
+      <button className={`navbar__item`} to='/' onClick={e => setExpanded(!expanded)}>
+        <img src={menu} alt="" />
         Menú
       </button>
 
--- src/views/public/home/Home.jsx
+++ src/views/public/home/Home.jsx
@@ -4,10 +4,35 @@ import Slider from '../../../components/public/slider/Slider'
 const Home = () => {
 
     return (
-        <main>
+        <main className='main'>
             <div className="home__slider">
                 <Slider />
             </div>
+            <section className='home__section'>
+                <div className="home__departamento home__slider">
+                    <img src="https://c4.wallpaperflare.com/wallpaper/540/853/301/girl-street-sparkler-firework-holiday-wallpaper-preview.jpg" alt="" />
+                </div>
+                <div className="home__categorias py-2 pt-4 overflow-x d-flex">
+                    <div className="home__categorias__item">
+                        <div className="card">
+                            <img src="https://cdn.shopify.com/s/files/1/2100/3755/products/Women_s-Active-Sports-Socks-White-Front.jpg?v=1614791239" className='shadow' alt="" />
+                            <h4 className='home__categorias__item__name'>Pantimedia gruesa</h4>
+                        </div>
+                    </div>
+                    <div className="home__categorias__item">
+                        <div className="card">
+                            <img src="https://cdn.shopify.com/s/files/1/2100/3755/products/Women_s-Active-Sports-Socks-White-Front.jpg?v=1614791239" className='shadow' alt="" />
+                            <h4 className='home__categorias__item__name'>Pantimedia gruesa</h4>
+                        </div>
+                    </div>
+                    <div className="home__categorias__item">
+                        <div className="card">
+                            <img src="https://cdn.shopify.com/s/files/1/2100/3755/products/Women_s-Active-Sports-Socks-White-Front.jpg?v=1614791239" className='shadow' alt="" />
+                            <h4 className='home__categorias__item__name'>Pantimedia gruesa</h4>
+                        </div>
+                    </div>
+                </div>
+            </section>
         </main>
     )
 }
