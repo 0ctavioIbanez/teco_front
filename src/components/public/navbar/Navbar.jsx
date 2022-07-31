a5c3845fc41aea7d241c84f14c6463570acc0ffa
0ctavioibanez <octaviogd@live.com.mx>
2022-07-30 22:00:26 -0500

    home in dev

--- src/components/public/navbar/Navbar.jsx
+++ src/components/public/navbar/Navbar.jsx
@@ -9,25 +9,42 @@ const Navbar = ({ expanded, setExpanded }) => {
   const path = window.location.pathname;
 
   return (
-    <div className='navbar__ d-flex align-items-center'>
-      <Link className={`navbar__item ${path === '/' ? 'active' : ''}`} to='/'>
-        <img src={home} alt="nav icon" />
-        Inicio
-      </Link>
-      <button className={`navbar__item ${path === '/search' ? 'active' : ''}`}>
-        <img src={search} alt="nav icon" />
-        Buscar
-      </button>
-      <Link className={`navbar__item ${path === '/cart' ? 'active' : ''}`} to='/'>
-        <img src={cart} alt="nav icon" />
-        Carrito
-      </Link>
-      <button className={`navbar__item`} to='/' onClick={e => setExpanded(!expanded)}>
-        <img src={menu} alt="nav icon" />
-        Menú
-      </button>
-
-    </div>
+    <>
+      <div className="topbar">
+        <div className="container d-flex justify-content-between align-items-center">
+          <div className="topbar__item">
+            <img src="https://upload.wikimedia.org/wikipedia/commons/2/2c/OneWeb_Logo.png" className='topbar__logo' alt="" />
+          </div>
+          <div className="topbar__item flex-grow-1">
+            <div className="topbar__search">
+              <input type="search" className='form-control' placeholder='Buscar...' />
+              <button>
+                <img src={search} alt="icon search" />
+              </button>
+            </div>
+          </div>
+          <div className="topbar__item"></div>
+        </div>
+      </div>
+      <div className='navbar__ d-flex align-items-center'>
+        <Link className={`navbar__item ${path === '/' ? 'active' : ''}`} to='/'>
+          <img src={home} alt="nav icon" />
+          Inicio
+        </Link>
+        <button className={`navbar__item ${path === '/search' ? 'active' : ''}`}>
+          <img src={search} alt="nav icon" />
+          Buscar
+        </button>
+        <Link className={`navbar__item ${path === '/cart' ? 'active' : ''}`} to='/'>
+          <img src={cart} alt="nav icon" />
+          Carrito
+        </Link>
+        <button className={`navbar__item`} to='/' onClick={e => setExpanded(!expanded)}>
+          <img src={menu} alt="nav icon" />
+          Menú
+        </button>
+      </div>
+    </>
   )
 }
 
--- src/views/public/home/Home.jsx
+++ src/views/public/home/Home.jsx
@@ -8,26 +8,30 @@ const Home = () => {
     return (
         <main className='main'>
             <div className="home__slider">
-                <Slider />
+                <Slider images={['https://img5.goodfon.com/wallpaper/nbig/d/1e/devushka-blondinka-sidit-ulybka-figura-nozhki-kolgotki-setka.jpg', 'https://cache.desktopnexus.com/thumbseg/1881/1881227-bigthumbnail.jpg']} />
             </div>
-            {items.map((item, i) =>
-                <section className='home__section' key={`deptos-${i}`}>
-                    <h5 className='section__title'>Departamentos</h5>
-                    <div className="home__departamento home__slider">
-                        <img src="https://c4.wallpaperflare.com/wallpaper/540/853/301/girl-street-sparkler-firework-holiday-wallpaper-preview.jpg" alt="cover" />
+
+            <section className='mt-5'>
+                <h2 className='text-center main-section-title public__text-secondary my-5'>¿De qué tienes <span className='public__text-primary'>ganas</span> hoy?</h2>
+                {items.map((item, i) =>
+                    <div className='home__section public__container' key={`deptos-${i}`}>
+                        <div className="home__departamento home__slider">
+                            <h5 className='section__title'>{item.departamento}</h5>
+                            <img src="https://thumbs.dreamstime.com/b/hermosa-chica-de-moda-con-el-pelo-largo-rizado-en-un-vestido-rosa-estudio-sobre-fondo-publicitario-publicidad-productos-para-212257109.jpg" alt="cover" />
+                        </div>
+                        <div className="home__categorias py-2 pt-4 overflow-x d-flex">
+                            {item.categorias.map((cat, c) =>
+                                <Link to={`tienda?d=${item.id}&c=${cat.id}`} className="home__categorias__item">
+                                    <div className="card">
+                                        <img src="https://cdn.shopify.com/s/files/1/2100/3755/products/Women_s-Active-Sports-Socks-White-Front.jpg?v=1614791239" className='shadow' alt="category" />
+                                        <h4 className='home__categorias__item__name'>{cat.categoria}</h4>
+                                    </div>
+                                </Link>
+                            )}
+                        </div>
                     </div>
-                    <div className="home__categorias py-2 pt-4 overflow-x d-flex">
-                        {item.categorias.map((cat, c) =>
-                            <Link to={`tienda?d=${item.id}&c=${cat.id}`} className="home__categorias__item">
-                                <div className="card">
-                                    <img src="https://cdn.shopify.com/s/files/1/2100/3755/products/Women_s-Active-Sports-Socks-White-Front.jpg?v=1614791239" className='shadow' alt="category" />
-                                    <h4 className='home__categorias__item__name'>{cat.categoria}</h4>
-                                </div>
-                            </Link>
-                        )}
-                    </div>
-                </section>
-            )}
+                )}
+            </section>
         </main>
     )
 }
