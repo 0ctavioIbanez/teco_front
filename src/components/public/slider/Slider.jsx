614449f04746d3f4c89cebf23cff79ddee96c4da
0ctavioibanez <octaviogd@live.com.mx>
2022-07-31 22:49:01 -0500

    responsive home done

--- src/components/public/slider/Slider.jsx
+++ src/components/public/slider/Slider.jsx
@@ -1,19 +1,44 @@
+import { useEffect, useState } from 'react';
 import useEmblaCarousel from 'embla-carousel-react'
+import Autoplay from 'embla-carousel-autoplay'
 import './slider.css';
 import { v4 } from 'uuid';
 
-const Slider = ({ images }) => {
-    const [emblaRef] = useEmblaCarousel();
+const Slider = ({ images, thumbs, config }) => {
+    const options = { delay: 1500 }
+    const autoplay = Autoplay(options);
+    let settings = [];
+
+    const setConfiguration = () => {
+        if (!config) {
+            return
+        }
+        if (config.autoplay) {
+            settings.push(autoplay);
+        }
+
+        if (emblaAPI) {
+            emblaAPI.reInit();
+        }
+    }
+
+    const [emblaRef, emblaAPI] = useEmblaCarousel({ loop: true }, settings);
+
+    useEffect(() => {
+      setConfiguration();
+    }, [config, emblaAPI])
+    
+
     return (
         <div className="embla" ref={emblaRef}>
             <div className="embla__container">
-                <div className="embla__slide shadow">
-                    {images ? images.map(image =>
-                        <img src={image} key={v4()} alt="slider" />
-                    )
-                        : null
-                    }
-                </div>
+                {images ? images.map(image =>
+                    <div className="embla__slide shadow" key={v4()}>
+                        <img src={image} alt="slider" />
+                    </div>
+                )
+                    : null
+                }
             </div>
         </div>
     )
--- src/views/public/home/Home.jsx
+++ src/views/public/home/Home.jsx
@@ -7,21 +7,26 @@ const Home = () => {
 
     return (
         <main className='main'>
-            <div className="home__slider">
-                <Slider images={['https://img5.goodfon.com/wallpaper/nbig/d/1e/devushka-blondinka-sidit-ulybka-figura-nozhki-kolgotki-setka.jpg', 'https://cache.desktopnexus.com/thumbseg/1881/1881227-bigthumbnail.jpg']} />
+            <div className="home__slider public__container">
+                <Slider 
+                    images={['https://img5.goodfon.com/wallpaper/nbig/d/1e/devushka-blondinka-sidit-ulybka-figura-nozhki-kolgotki-setka.jpg', 'https://cache.desktopnexus.com/thumbseg/1881/1881227-bigthumbnail.jpg']}
+                    config={{
+                        autoplay: true
+                    }}
+                 />
             </div>
 
-            <section className='mt-5'>
-                <h2 className='text-center main-section-title public__text-secondary my-5'>¿De qué tienes <span className='public__text-primary'>ganas</span> hoy?</h2>
+            <section>
+                <h2 className='text-center main-section-title public__text-secondary'>¿De qué tienes <span className='public__text-primary'>ganas</span> hoy?</h2>
                 {items.map((item, i) =>
                     <div className='home__section public__container' key={`deptos-${i}`}>
                         <div className="home__departamento home__slider">
-                            <h5 className='section__title'>{item.departamento}</h5>
+                            <h5 className={`section__title ${i % 2 === 0 ? 'left' : 'right'}`}>{item.departamento}</h5>
                             <img src="https://thumbs.dreamstime.com/b/hermosa-chica-de-moda-con-el-pelo-largo-rizado-en-un-vestido-rosa-estudio-sobre-fondo-publicitario-publicidad-productos-para-212257109.jpg" alt="cover" />
                         </div>
                         <div className="home__categorias py-2 pt-4 overflow-x d-flex">
                             {item.categorias.map((cat, c) =>
-                                <Link to={`tienda?d=${item.id}&c=${cat.id}`} className="home__categorias__item">
+                                <Link to={`tienda?d=${item.id}&c=${cat.id}`} key={`dep-${c}-${cat.id}`} className="home__categorias__item">
                                     <div className="card">
                                         <img src="https://cdn.shopify.com/s/files/1/2100/3755/products/Women_s-Active-Sports-Socks-White-Front.jpg?v=1614791239" className='shadow' alt="category" />
                                         <h4 className='home__categorias__item__name'>{cat.categoria}</h4>
